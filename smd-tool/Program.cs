2798a2c99092fa05621d0d6b6d16022dbe558ddb
LONELY-WOLF <zvenayte@pisem.net>
2013-09-02 17:00:09 +0400

    first commit

---
+++ smd-tool/Program.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace smd_tool
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Count() > 0)
+            {
+                switch (args[0])
+                {
+                    case "/u":
+                        {
+                            //Unpack
+                            if (args.Count() >= 2)
+                            {
+                                if (File.Exists(args[1]))
+                                {
+                                    string dir = "";
+                                    if (args.Count() >= 3)
+                                    {
+                                        if (Directory.Exists(args[2]))
+                                        {
+                                            dir = args[2];
+                                        }
+                                        else
+                                        {
+                                            PrintError("Directory not found!");
+                                            return -1;
+                                        }
+                                    }
+                                    if (dir == "") dir = Environment.CurrentDirectory;
+                                    FileStream smd = File.OpenRead(args[1]);
+                                    byte[] buffer = new byte[0x800];
+                                    smd.Read(buffer, 0, 0x800);
+                                    File.WriteAllBytes(Path.Combine(dir, "\\header.bin"), buffer);
+                                    SMDPart part = new SMDPart();
+                                    int offset = 0x200;
+                                    Console.ForegroundColor = ConsoleColor.White;
+                                    Console.WriteLine("Partition name   N Offset N Length R Offset R Length Part. ID FS ID");
+                                    Console.ResetColor();
+                                    while (buffer[offset] != 0)
+                                    {
+                                        part.ReadHeader(buffer, offset);
+                                        Console.Write(part.Name.PadRight(17));
+                                        Console.Write("{0:X8} {1:X8} {2:X8} {3:X8} ", part.NandOffset, part.NandLenth, part.FileOffset, part.FileLenth);
+                                        Console.Write("{0:X8} {1:X8} [      ]", part.ID, part.FSType);
+                                        if (part.Extract(smd, dir))
+                                        {
+                                            Console.CursorLeft -= 5;
+                                            Console.ForegroundColor = ConsoleColor.Green;
+                                            Console.Write("OK");
+                                            Console.ResetColor();
+                                            Console.CursorLeft += 3;
+                                        }
+                                        else
+                                        {
+                                            Console.CursorLeft -= 7;
+                                            Console.ForegroundColor = ConsoleColor.Red;
+                                            Console.Write("Failed");
+                                            Console.ResetColor();
+                                            Console.CursorLeft += 1;
+                                        }
+                                        Console.WriteLine();
+                                        offset += 0x40;
+                                    }
+                                    return 0;
+                                }
+                                else
+                                {
+                                    PrintError("SMD file not found!");
+                                    return -1;
+                                }
+                            }
+                            break;
+                        }
+                    case "/p":
+                        {
+                            //Pack
+                            Console.WriteLine("Not implelemented yet");
+                            return -1;
+                            break;
+                        }
+                    case "/h":
+                        {
+                            //Parse header
+                            if (args.Count() >= 2)
+                            {
+                                if (File.Exists(args[1]))
+                                {
+                                    FileStream smd = File.OpenRead(args[1]);
+                                    byte[] buffer = new byte[0x800];
+                                    smd.Read(buffer, 0, 0x800);
+                                    SMDPart part = new SMDPart();
+                                    int offset = 0x200;
+                                    Console.ForegroundColor = ConsoleColor.White;
+                                    Console.WriteLine("Partition name   N Offset N Length R Offset R Length Part. ID FS ID");
+                                    Console.ResetColor();
+                                    while (buffer[offset] != 0)
+                                    {
+                                        part.ReadHeader(buffer, offset);
+                                        Console.Write(part.Name.PadRight(17));
+                                        Console.Write("{0:X8} {1:X8} {2:X8} {3:X8} ", part.NandOffset, part.NandLenth, part.FileOffset, part.FileLenth);
+                                        Console.Write("{0:X8} {1:X8} [      ]", part.ID, part.FSType);
+                                        if ((part.FileOffset != 0) & (part.FileLenth != 0))
+                                        {
+                                            Console.CursorLeft -= 5;
+                                            Console.ForegroundColor = ConsoleColor.Green;
+                                            Console.Write("OK");
+                                            Console.ResetColor();
+                                            Console.CursorLeft += 3;
+                                        }
+                                        else
+                                        {
+                                            Console.CursorLeft -= 7;
+                                            Console.ForegroundColor = ConsoleColor.Red;
+                                            Console.Write("Failed");
+                                            Console.ResetColor();
+                                            Console.CursorLeft += 1;
+                                        }
+                                        Console.WriteLine();
+                                        offset += 0x40;
+                                    }
+                                    return 0;
+                                }
+                                else
+                                {
+                                    PrintError("SMD file not found!");
+                                    return -1;
+                                }
+                            }
+                            break;
+                        }
+                    default:
+                        {
+                            PrintHelp();
+                            return -1;
+                            break;
+                        }
+                }
+            }
+            //Unknown arguments
+            PrintHelp();
+            return -1;
+        }
+
+        static void PrintHelp()
+        {
+            Console.Write(@"
+USAGE:
+
+smd-tool /u <file> [<path>]
+    Unpack SMD file
+
+smd-tool /p <file> [<path>]
+    Pack partitions located at <path> into SMD file <file>
+
+smd-tool /h <file>
+    Parse header in <file> and display info
+
+Default <path> is current directory
+");
+        }
+
+        static void PrintError(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
---
+++ smd-tool/SMDPart.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace smd_tool
+{
+    class SMDPart
+    {
+        public UInt32 NandOffset, NandLenth, FileOffset, FileLenth, FSType, ID, IsPresentSignature;
+        public string Name;
+        public byte[] MD5;
+
+        public SMDPart() { }
+
+        public void ReadHeader(byte[] header, int offset)
+        {
+            Name = Encoding.ASCII.GetString(header, offset, 0x10).TrimEnd(new char[] { '\0' });
+            NandOffset = BitConverter.ToUInt32(header, offset + 0x10);
+            NandLenth = BitConverter.ToUInt32(header, offset + 0x14);
+            FileOffset = BitConverter.ToUInt32(header, offset + 0x18);
+            FileLenth = BitConverter.ToUInt32(header, offset + 0x1C);
+            IsPresentSignature = BitConverter.ToUInt32(header, offset + 0x20);
+            ID = BitConverter.ToUInt32(header, offset + 0x24);
+            FSType = BitConverter.ToUInt32(header, offset + 0x28);
+            MD5 = header.Skip(offset + 0x30).Take(0x10).ToArray();
+        }
+
+        public bool Extract(FileStream smd, string path)
+        {
+            if ((IsPresentSignature == 0x1F1F1F1F) & (FileOffset != 0) & (FileLenth != 0))
+            {
+                try
+                {
+                    FileStream outFile = File.Open(Path.Combine(path, Name + ".bin"), FileMode.Create);
+                    byte[] buffer = new byte[1024 * 1024];
+                    smd.Position = FileOffset;
+                    int count;
+                    while (true)
+                    {
+                        count = (int)(FileOffset + FileLenth - smd.Position);
+                        if (count == 0)
+                        {
+                            break;
+                        }
+                        if (count > buffer.Length) count = buffer.Length;
+                        int read = smd.Read(buffer, 0, count);
+                        if (read <= 0)
+                        {
+                            break;
+                        }
+                        outFile.Write(buffer, 0, read);
+                    }
+                    outFile.Close();
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
