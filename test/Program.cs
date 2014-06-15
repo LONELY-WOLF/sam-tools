652751d96086231ce23973f5241cbf9da371f92f
LONELY-WOLF <zvenayte@pisem.net>
2014-06-15 18:35:07 +0400

    console test project added

---
+++ test/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using sam_unpack_lib;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MBN mbnFile = new MBN();
+            mbnFile.Load("1.mbn");
+            foreach (MBN.Section item in mbnFile.Sections)
+            {
+                mbnFile.ExtractSection(item, "csc");
+            }
+        }
+    }
+}
