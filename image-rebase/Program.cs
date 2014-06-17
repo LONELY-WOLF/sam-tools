cd48ce3f5b8d9ffaf904d1d278083b885b782d99
LONELY-WOLF <zvenayte@pisem.net>
2014-06-18 01:26:44 +0400

    [image-rebase] cmdline improvements

--- image-rebase/Program.cs
+++ image-rebase/Program.cs
@@ -9,163 +9,161 @@ namespace image_rebase
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
             string binFileName = null, imgFileName = null;
             uint partCount = 0;
             long headerPosition = 0x10, binFilePos = 8, imgFilePos, partSize, zeroSize;
             long sectorSize = 512;
 
-            if (args.Length >= 1)
+            binFileName = GetArg(args, "/u", null);
+            if (binFileName != null)
             {
-                if (args.Length >= 2)
+                imgFileName = GetArg(args, "/o", null);
+                if (imgFileName == null)
                 {
-                    binFileName = args[1];
-                    if (args.Length >= 3)
+                    if (binFileName.EndsWith(".bin"))
                     {
-                        imgFileName = args[3];
+                        imgFileName = binFileName.Substring(0, binFileName.Length - 4) + ".img";
                     }
                     else
                     {
-                        if (binFileName.EndsWith(".bin"))
-                        {
-                            imgFileName = binFileName.Substring(0, binFileName.Length - 4) + ".img";
-                        }
-                        else
-                        {
-                            imgFileName = binFileName + ".img";
-                        }
+                        imgFileName = binFileName + ".img";
                     }
                 }
-                switch (args[0])
+                FileStream binFile = new FileStream(binFileName, FileMode.Open);
+                FileStream imgFile = new FileStream(imgFileName, FileMode.Create);
+                byte[] buffer = new byte[8];
+                binFile.Read(buffer, 0, 8);
+                if (BitConverter.ToUInt64(buffer, 0) != 0x7260EACCEACC9442)
                 {
-                    case "/u":
-                        {
-                            if (string.IsNullOrWhiteSpace(binFileName))
-                            {
-                                PrintError("Invalid input file name!");
-                                return;
-                            }
-                            FileStream binFile = new FileStream(binFileName, FileMode.Open);
-                            FileStream imgFile = new FileStream(imgFileName, FileMode.Create);
-                            byte[] buffer = new byte[8];
-                            binFile.Read(buffer, 0, 8);
-                            if (BitConverter.ToUInt64(buffer, 0) != 0x7260EACCEACC9442)
-                            {
-                                PrintError("File signature mismatch!");
-                                return;
-                            }
-                            binFile.Read(buffer, 0, 4);
-                            partCount = BitConverter.ToUInt32(buffer, 0);
-                            Console.WriteLine("Parts count: {0}", partCount);
-                            Console.ForegroundColor = ConsoleColor.White;
-                            Console.WriteLine("     №   From     To       Count");
-                            Console.ResetColor();
-                            for (int i = 0; i < partCount; i++)
-                            {
-                                binFile.Position = headerPosition;
-                                binFile.Read(buffer, 0, 8);
-                                imgFilePos = BitConverter.ToUInt32(buffer, 0);
-                                partSize = BitConverter.ToUInt32(buffer, 4);
-                                Console.WriteLine("Part {0:d4} {1:X8} {2:X8} {3:X8}", i, binFilePos, imgFilePos, partSize);
-                                zeroSize = (imgFilePos * sectorSize) - imgFile.Position;
-                                if (zeroSize != 0)
-                                {
-                                    FileIO.WriteZeroes(imgFile, imgFile.Position, zeroSize);
-                                }
-                                FileIO.StreamCopy(binFile, imgFile, binFilePos * sectorSize, partSize * sectorSize, 0x100000);
-                                binFilePos += partSize;
-                                headerPosition += 8;
-                            }
-                            break;
-                        }
-                    case "/p":
-                        {
-                            PrintError("Not implemented yet");
-                            break;
-                        }
-                    case "/h":
-                        {
-                            if (string.IsNullOrWhiteSpace(binFileName))
-                            {
-                                PrintError("Invalid input file name!");
-                                return;
-                            }
-                            FileStream binFile = new FileStream(binFileName, FileMode.Open);
-                            FileStream imgFile = new FileStream(imgFileName, FileMode.Create);
-                            byte[] buffer = new byte[8];
-                            binFile.Read(buffer, 0, 8);
-                            if (BitConverter.ToUInt64(buffer, 0) != 0x7260EACCEACC9442)
-                            {
-                                PrintError("File signature mismatch!");
-                                return;
-                            }
-                            binFile.Read(buffer, 0, 4);
-                            partCount = BitConverter.ToUInt32(buffer, 0);
-                            Console.WriteLine("Parts count: {0}", partCount);
-                            Console.ForegroundColor = ConsoleColor.White;
-                            Console.WriteLine("     №   From     To       Count");
-                            Console.ResetColor();
-                            for (int i = 0; i < partCount; i++)
-                            {
-                                binFile.Position = headerPosition;
-                                binFile.Read(buffer, 0, 8);
-                                imgFilePos = BitConverter.ToUInt32(buffer, 0);
-                                partSize = BitConverter.ToUInt32(buffer, 4);
-                                Console.WriteLine("Part {0:d3} {1:X8} {2:X8} {3:X8}", i, binFilePos, imgFilePos, partSize);
-                                zeroSize = (imgFilePos * sectorSize) - imgFile.Position;
-                                binFilePos += partSize;
-                                headerPosition += 8;
-                            }
-                            break;
-                        }
-                    default:
-                        {
-                            //usage
-                            Console.WriteLine(@"
-USAGE:
-
-image-rebase /u <input file> [<output file>]
-    Extract image from <input file> to <output file>
-
-image-rebase /p <input file> [<output file>]
-    Pack image <input file> to <output file>
-
-image-rebase /h <file>
-    Parse header in <file> and display info
-
-(c) -WOLF- 2013
-");
-                            break;
-                        }
+                    PrintError("File signature mismatch!");
+                    return -1;
                 }
+                binFile.Read(buffer, 0, 4);
+                partCount = BitConverter.ToUInt32(buffer, 0);
+                Console.WriteLine("Parts count: {0}", partCount);
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("     №   From     To       Count");
+                Console.ResetColor();
+                for (int i = 0; i < partCount; i++)
+                {
+                    binFile.Position = headerPosition;
+                    binFile.Read(buffer, 0, 8);
+                    imgFilePos = BitConverter.ToUInt32(buffer, 0);
+                    partSize = BitConverter.ToUInt32(buffer, 4);
+                    Console.WriteLine("Part {0:d4} {1:X8} {2:X8} {3:X8}", i, binFilePos, imgFilePos, partSize);
+                    zeroSize = (imgFilePos * sectorSize) - imgFile.Position;
+                    if (zeroSize != 0)
+                    {
+                        FileIO.WriteZeroes(imgFile, imgFile.Position, zeroSize);
+                    }
+                    FileIO.StreamCopy(binFile, imgFile, binFilePos * sectorSize, partSize * sectorSize, 0x100000);
+                    binFilePos += partSize;
+                    headerPosition += 8;
+                }
+                return 0;
             }
-            else
+            binFileName = GetArg(args, "/p", null);
+            if (binFileName != null)
             {
-                //usage
-                Console.WriteLine(@"
-USAGE:
-
-image-rebase /u <input file> [<output file>]
-    Extract image from <input file> to <output file>
-
-image-rebase /p <input file> [<output file>]
-    Pack image <input file> to <output file>
-
-image-rebase /h <file>
-    Parse header in <file> and display info
-
-(c) -WOLF- 2013
-");
+                PrintError("Not implemented yet");
+                return -1;
             }
+            binFileName = GetArg(args, "/info", null);
+            if (binFileName != null)
+            {
+                FileStream binFile = new FileStream(binFileName, FileMode.Open);
+                FileStream imgFile = new FileStream(imgFileName, FileMode.Create);
+                byte[] buffer = new byte[8];
+                binFile.Read(buffer, 0, 8);
+                if (BitConverter.ToUInt64(buffer, 0) != 0x7260EACCEACC9442)
+                {
+                    PrintError("File signature mismatch!");
+                    return -1;
+                }
+                binFile.Read(buffer, 0, 4);
+                partCount = BitConverter.ToUInt32(buffer, 0);
+                Console.WriteLine("Parts count: {0}", partCount);
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("     №   From     To       Count");
+                Console.ResetColor();
+                for (int i = 0; i < partCount; i++)
+                {
+                    binFile.Position = headerPosition;
+                    binFile.Read(buffer, 0, 8);
+                    imgFilePos = BitConverter.ToUInt32(buffer, 0);
+                    partSize = BitConverter.ToUInt32(buffer, 4);
+                    Console.WriteLine("Part {0:d3} {1:X8} {2:X8} {3:X8}", i, binFilePos, imgFilePos, partSize);
+                    zeroSize = (imgFilePos * sectorSize) - imgFile.Position;
+                    binFilePos += partSize;
+                    headerPosition += 8;
+                }
+                return 0;
+            }
+            PrintUsage();
+            return -1;
         }
-
 
         static void PrintError(string message)
         {
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine(message);
             Console.ResetColor();
+        }
+
+        static void PrintUsage()
+        {
+            //usage
+            Console.WriteLine(@"
+USAGE:
+
+image-rebase /u <input file> [/o <output file>]
+    Extract image from <input file> to <output file>
+
+image-rebase /p <input file> [/o <output file>]
+    Pack image <input file> to <output file>
+
+image-rebase /info <file>
+    Parse header in <file> and display info
+
+(c) -WOLF- 2013
+");
+        }
+
+        static string GetArg(string[] args, string name, string defaultValue)
+        {
+            int index = -2;
+            int count = args.Count();
+            for (int i = 0; i < count; i++)
+            {
+                if (args[i] == name)
+                {
+                    index = i;
+                    break;
+                }
+            }
+            if ((index + 1 == count) || (index < 0))
+            {
+                return defaultValue;
+            }
+            else
+            {
+                return args[index + 1];
+            }
+        }
+
+        static bool HasFlag(string[] args, string name)
+        {
+            int count = args.Count();
+            for (int i = 0; i < count; i++)
+            {
+                if (args[i] == name)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
